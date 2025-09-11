ca080a99f4acd348435833f5809d361c90e1854a
Azam F <115250689+OriExe@users.noreply.github.com>
2025-09-11 22:55:18 +0100

    Adding Md covert to html code

---
+++ Portfolio/src/Pages/AcademicProjects.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import {extractProjectData} from "@/mdExtract.js"
+// state
+const projectData = ref([]);
+
+onMounted(async () => {
+  const res = await fetch("/pages/project.md"); // must be in /public
+  const markdown = await res.text();
+  projectData.value = extractProjectData(markdown);
+  console.log(projectData.value);
+});
+</script>
+
+<template>
+    <div class="slide" data-anchor="slide2">
+        <h2 class="content">Academic Projects</h2>
+    </div>
+</template>
