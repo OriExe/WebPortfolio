7723074746b599f1f7f213ed01d9507981776c85
Azam F <115250689+OriExe@users.noreply.github.com>
2026-08-11 16:21:37 +0100

    Fix issue deloying to github pages

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,23 +1,10 @@
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import { loadEnv } from "vite";
 import spectre, { type GiscusMapping } from "./package/src";
 import { spectreDark } from "./src/ec-theme";
-
-const {
-	GISCUS_REPO,
-	GISCUS_REPO_ID,
-	GISCUS_CATEGORY,
-	GISCUS_CATEGORY_ID,
-	GISCUS_MAPPING,
-	GISCUS_STRICT,
-	GISCUS_REACTIONS_ENABLED,
-	GISCUS_EMIT_METADATA,
-	GISCUS_LANG,
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
@@ -46,9 +33,7 @@ const config = defineConfig({
 			},
 		}),
 	],
-	adapter: node({
-		mode: "standalone",
-	}),
+	
 });
 
 export default config;
